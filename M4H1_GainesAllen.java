386efedcb2d957e0d5d06ce3a08e4a14f17acb71
Allen-Gaines <allenjgaines@gmail.com>
2025-03-24 11:11:53 -0400

    Add files via upload

---
+++ M4H1_GainesAllen.java
@@ -0,0 +1,70 @@
+// Allen Gaines
+
+// M4HW1
+
+// 03/17/2025
+
+// A program that processes commission rates 
+
+import java.util.Scanner;
+
+
+
+public class M4H1_GainesAllen {
+
+    
+
+
+    public static void main(String[] args) 
+    {
+        //commision rate structure 
+        int widgetsSold;
+        int returnWidgets;
+        int basesalary = 2000;
+        double commRate;
+        
+        double WIGITSOLDAMMOUNT = 4.79;
+
+        
+        
+       
+        
+        Scanner sold = new Scanner(System.in);
+        System.out.print("Enter the number of widgets sold: " );
+        widgetsSold = sold.nextInt();
+        System.out.print("Enter the number of returns: ");
+        returnWidgets = sold.nextInt();
+
+        int netWidgetsSold = widgetsSold - returnWidgets; 
+
+
+
+
+
+        //if else structure 
+        if (netWidgetsSold >=0 && netWidgetsSold <=100 ) 
+            commRate = .1;
+        
+        else if(netWidgetsSold <=199 ) 
+            commRate = .15; 
+        else if (netWidgetsSold <=299)
+            commRate = .20;
+        else 
+            commRate = .25;
+        double widgetSaleAmmount = netWidgetsSold *WIGITSOLDAMMOUNT;
+        double commAmmount = commRate * widgetSaleAmmount ;
+        double Monthlysalary =commAmmount + basesalary;
+        
+        
+
+
+        System.out.println("Sales person: Allen Gaines");
+        System.out.println("net witdets sold:" + netWidgetsSold);
+        System.out.println("widget sales ammount: " + widgetSaleAmmount);
+        System.out.println("commision ammount: " +commAmmount);
+        
+        System.out.println("monthly salary: " + Monthlysalary);
+
+    }
+
+}
